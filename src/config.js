0b08bc228dbf367be9fc5d9867b51619cb3b0dbb
joelblessed <joelblessedgod@gmail.com>
2025-05-08 14:44:00 +0000

    rout

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 
-export const api = "https://apaxt-api.onrender.com";
+export const api = "https://apaxt2-api.onrender.com";
 // "http://localhost:5000",
-// https://apaxt-api.onrender.com
+// https://apaxt2-api.onrender.com
+
 
 // "typescript": "^5.8.2",
 // "typescript": "^4.9.5",
